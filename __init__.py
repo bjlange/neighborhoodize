5078a2ddc6e04ac21245688170a1e6d796d8dbf4
Brian Lange <brian.lange@datascopeanalytics.com>
2015-05-12 16:22:13 -0500

    Add initial implementation

--- __init__.py
+++ __init__.py
@@ -2,25 +2,85 @@
 neighborhood they're located in. Currently Chicago only."""
 
 # imports
+import os
 import sys
+import pygeoif
+import shapely
+from bs4 import BeautifulSoup
+from fastkml import kml
 
 # constants
-
+NEIGHBORHOOD_FILENAME = "Boundaries - Neighborhoods.kml"
+PATH_TO_THIS_FILE = os.path.dirname(os.path.abspath(__file__))
+NEIGHBORHOOD_KML = os.path.join(PATH_TO_THIS_FILE,
+                                NEIGHBORHOOD_FILENAME)
 # exception classes
 
 # interface functions
-def read_kml_to_polygon(filename):
-    """Takes a path to a KML file. Returns a (list of?) Shapely polygons
+def read_neighborhood_kml(filename, meta_key="PRI_NEIGH"):
+    """Takes a path to a KML file. Returns a dictionary of Shapely
+    polygons keyed by specified metadata field (default: primary
+    neighborhood).
     TODO: improve docstring
     """
-    raise NotImplementedError
+    with open(filename) as kml_file:
+        kml_obj = kml.KML()
+        kml_obj.from_string(kml_file.read())
+
+    document = list(kml_obj.features())[0]
+    hood_polys = {}
+
+    for placemark in document.features():
+        # print "EXTENDED"
+        # print placemark.extended_data
+        # print "DESC"
+        metadata = BeautifulSoup(placemark.description)
+        attr_names = [el.text for el in metadata.find_all('span', 'atr-name')]
+        attr_vals = [el.text for el in metadata.find_all('span', 'atr-value')]
+
+        meta_dict = dict(zip(attr_names, attr_vals))
+        # example:
+        # {u'PRI_NEIGH': u'Austin',
+        #  u'SEC_NEIGH': u'AUSTIN',
+        #  u'SHAPE_AREA': u'1.70037750826E8',
+        #  u'SHAPE_LEN': u'55473.345911',
+        #  u'_SocrataID': u'2avh-wj5s'}
+
+        poly = [g for g in placemark.geometry.geoms
+                if isinstance(g, pygeoif.geometry.Polygon)][0]
+        shape_reader = shapely.geos.WKTReader(shapely.geos.lgeos)
+        shape_poly = shape_reader.read(poly.wkt)
+
+        hood_polys[meta_dict[meta_key]] = shape_poly
+    return hood_polys
 
 # classes
+class Neighborhoodizer(object):
+
+    def __init__(self):
+        self.hood_polys = read_neighborhood_kml(NEIGHBORHOOD_KML)
+
+    def get_neighborhoods(self, lat, lng):
+        """Takes a latitude and a longitude and returns a list of matching
+        neighborhood names."""
+        point = shapely.geometry.Point(lng, lat)
+        hoods = [name for name, poly in self.hood_polys.iteritems()
+                 if poly.contains(point)]
+        return hoods
 
 # internal functions & classes
 
 
 def main():
+    izer = Neighborhoodizer()
+    lat, lng = 41.925495, -87.705847
+    print "Should be Logan Square:"
+    print izer.get_neighborhoods(lat, lng)
+
+    lat, lng = 41.927299, -87.646270
+    print "should be Lincoln Park:"
+    print izer.get_neighborhoods(lat, lng)
+
     return 0
 
 if __name__ == "__main__":
