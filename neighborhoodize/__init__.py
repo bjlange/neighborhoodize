7a1cb6377796405bdfab44877f35f04f8d94c7e1
Brian Lange <brian.lange@datascopeanalytics.com>
2015-09-01 15:08:06 -0500

    Restructuring to deal with additional cities/boundaries

--- neighborhoodize/__init__.py
+++ neighborhoodize/__init__.py
@@ -14,17 +14,18 @@ from fastkml import kml
 PATH_TO_THIS_FILE = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(PATH_TO_THIS_FILE, 'data')
 
+# relevant meta_key: PRI_NEIGH
 CHICAGO_NEIGHBORHOODS = os.path.join(DATA_DIR,
                                      "Boundaries - Neighborhoods.kml")
+# relevant meta_key: NTAName
 NYC_NEIGHBORHOODS = os.path.join(DATA_DIR,
                                  "NYC - Neighborhood Tabulation Areas.kml")
-
 
 
 # exception classes
 
 # interface functions
-def read_neighborhood_kml(filename, meta_key="PRI_NEIGH"):
+def read_neighborhood_kml(filename, map_name, meta_key="PRI_NEIGH"):
     """Takes a path to a KML file. Returns a dictionary of Shapely
     polygons keyed by specified metadata field (default: primary
     neighborhood).
@@ -35,7 +36,8 @@ def read_neighborhood_kml(filename, meta_key="PRI_NEIGH"):
         kml_obj.from_string(kml_file.read())
 
     document = list(kml_obj.features())[0]
-    hood_polys = {}
+
+    hood_map = NeighborhoodMap(map_name)
 
     for placemark in document.features():
         # print "EXTENDED"
@@ -58,45 +60,65 @@ def read_neighborhood_kml(filename, meta_key="PRI_NEIGH"):
         shape_reader = shapely.geos.WKTReader(shapely.geos.lgeos)
         shape_poly = shape_reader.read(poly.wkt)
 
-        hood_polys[meta_dict[meta_key]] = shape_poly
-    return hood_polys
+        hood = Neighborhood(meta_dict[meta_key], shape_poly, kml_meta=meta_dict)
+        hood_map.neighborhoods.append(hood)
+
+    return hood_map
+
 
 # classes
 class Neighborhoodizer(object):
-
     def __init__(self):
-        self.hood_polys = read_neighborhood_kml(CHICAGO_NEIGHBORHOODS)
+        self.hood_map = read_neighborhood_kml(NYC_NEIGHBORHOODS, "NYC", meta_key="NTAName")
 
     def get_neighborhoods(self, lat, lng):
         """Takes a latitude and a longitude and returns a list of matching
         neighborhood names."""
         point = shapely.geometry.Point(lng, lat)
-        hoods = [name for name, poly in self.hood_polys.iteritems()
-                 if poly.contains(point)]
+        hoods = [hood.name for hood in self.hood_map.neighborhoods
+                 if hood.polygon.contains(point)]
         return hoods
+
 
 class NeighborhoodMap(object):
     """A NeighborhoodMap is a series of polygons representing
     neighborhoods, constructed from a file (KML only for the time
     being)"""
+    neighborhoods = []
+
+    def __init__(self, map_name):
+        self.name = map_name
 
 
-    pass
+class Neighborhood(object):
+    """A Neighborhood is a polygon defining geographic borders, with accompanying metadata.
+    """
+
+    def __init__(self, neighborhood_name, polygon, kml_meta=None):
+        self.name = neighborhood_name
+        self.polygon = polygon
+        self.kml_meta = kml_meta
+
 
 # internal functions & classes
 
 
 def main():
     izer = Neighborhoodizer()
-    lat, lng = 41.925495, -87.705847
-    print "Should be Logan Square:"
-    print izer.get_neighborhoods(lat, lng)
+    # lat, lng = 41.925495, -87.705847
+    # print "Should be Logan Square:"
+    # print izer.get_neighborhoods(lat, lng)
+    #
+    # lat, lng = 41.927299, -87.646270
+    # print "should be Lincoln Park:"
+    # print izer.get_neighborhoods(lat, lng)
 
-    lat, lng = 41.927299, -87.646270
-    print "should be Lincoln Park:"
+    lat, lng = 40.709467, -73.962600
+    print "should be Williamsburg:"
     print izer.get_neighborhoods(lat, lng)
 
     return 0
+
 
 if __name__ == "__main__":
     STATUS_CODE = main()
