633aefbbdbf99ecd07571cccb15717288b016a53
Brian Lange <brian.lange@datascopeanalytics.com>
2015-09-04 17:40:49 -0500

    Updating metadata

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 
 setup(name="Neighborhoodize",
       version='0.9',
-      description='Utility for translating lat, long coordinates into Chicago '
-                  'neighborhoods',
+      description='Utility for translating lat, long coordinates into '
+                  'neighborhoods in various cities',
       author='Brian Lange',
       author_email='brian.lange@datascopeanalytics.com',
       url='https://github.com/bjlange/neighborhoodize',
